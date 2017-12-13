c3a21c2dcaafe8e6859ebb5825a750f2715afa81
sergey@telminov.ru <sergey@telminov.ru>
2017-12-13 21:27:38 +0300

    добавил get_cleaned_data

--- sw_excel_parser/parsers.py
+++ sw_excel_parser/parsers.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import xlrd
 from xlrd.sheet import Sheet
@@ -16,7 +16,7 @@ class Parser:
         else:
             raise ValueError('You must provide workbook or file_contents')
 
-        self.sheet_items =  OrderedDict()
+        self.sheet_items = OrderedDict()
 
     def __iter__(self):
         return iter(self.sheet_items.items())
@@ -33,7 +33,7 @@ class Parser:
     def get_sheets(self) -> List[Sheet]:
         return self.workbook.sheets()
 
-    def find_headers(self) -> Dict[Sheet, int or None]:
+    def find_headers(self) -> Dict[Sheet, Optional[int]]:
         sheets = self.get_sheets()
         fields = self.item._unbound_fields.values()
         header = {field.kwargs['header'].lower() for field in fields}
@@ -80,3 +80,17 @@ class Parser:
         headers_nrows = self.find_headers().values()
 
         return any(nrow is not None for nrow in headers_nrows)
+
+    def get_cleaned_data(self):
+        cleaned_data = OrderedDict()
+
+        for sheet, items in self.sheet_items.items():
+            sheet_name = sheet.name
+            sheet_data = []
+            for item in items:
+                if item.cleaned_data:
+                    sheet_data.append(item.cleaned_data)
+
+            cleaned_data[sheet_name] = sheet_data
+
+        return cleaned_data
